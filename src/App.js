c73e3ff2bf90de2410955700459a898f850d41d2
Ole Lynge Soerensen <olelynge@gmail.com>
2024-08-08 18:49:31 +0200

    Change front page text and links.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my website.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/uncas"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View my github repositories.
+        </a>
+        <a
+          className="App-link"
+          href="https://olelynge.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View my blog.
         </a>
       </header>
     </div>
